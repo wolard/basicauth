bdf62c742c467cb966eebaa7b074a89a5e4e48bb
Klaus Sandell <wolard@gmail.com>
2021-07-04 23:38:31 +0300

    auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
+const auth = require('./auth');
 const app = express();
 
 app.use(morgan('dev'));
@@ -9,6 +10,7 @@ app.get('/',(req,res)=>{
     res.statusCode=200;
     res.end("Welcome to your express app!")
 })
+app.use(auth)
 app.get('/secret',(req,res)=>{
     res.statusCode=200;
     res.end("******")
