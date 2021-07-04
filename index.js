38aa97e0fdb609f044fb71994e73b07e32356e41
wolard <wolard@gmail.com>
2021-07-04 22:17:38 +0300

    server code

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-
 app.use(morgan('dev'));
 
-
+app.get('/',(req,res)=>{
+    res.statusCode=200;
+    res.end("Welcome to your express app!")
+})
+app.get('/secret',(req,res)=>{
+    res.statusCode=200;
+    res.end("******")
+});
 const server = http.createServer(app);
 
 server.listen(3001,'localhost',()=>{
