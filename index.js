a091cf94a3baa403ce931d772ec3e01999eafec3
wolard <wolard@gmail.com>
2021-07-05 08:52:56 +0300

    "auth added"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
+const auth = require('./auth');
 const app = express();
 
 app.use(morgan('dev'));
@@ -9,10 +10,12 @@ app.get('/',(req,res)=>{
     res.statusCode=200;
     res.end("Welcome to your express app!")
 })
+app.use(auth)
 app.get('/secret',(req,res)=>{
     res.statusCode=200;
     res.end("******")
 });
+
 const server = http.createServer(app);
 
 server.listen(3001,'localhost',()=>{
